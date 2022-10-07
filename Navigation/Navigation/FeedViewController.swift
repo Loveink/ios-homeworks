78cb3cc23afb07cbcca6a27a928861faf64e2acb
Loveink <106765014+Loveink@users.noreply.github.com>
2022-10-07 21:29:38 +0300

    homework 2.2 done
    add new constraints to ProfileViewController. add new button. in FeedViewController add vertical stack with 2 buttons (touch go to PostViewController)

--- Navigation/Navigation/FeedViewController.swift
+++ Navigation/Navigation/FeedViewController.swift
@@ -8,35 +8,62 @@
 import UIKit
 
 class FeedViewController: UIViewController {
-    
     var postOne = Post(text: "POST ONE")
-    
+    private lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    private lazy var postButtonOne: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(self.goToPost), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.backgroundColor = .systemMint
+        button.layer.cornerRadius = 12
+        button.setTitle("Go to post 1", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private lazy var postButtonTwo: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(self.goToPost), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.backgroundColor = .systemIndigo
+        button.layer.cornerRadius = 12
+        button.setTitle("Go to post 2", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
-        
-        let uiButton = UIButton()
-        self.view.addSubview(uiButton)
-        
-        uiButton.addAction(UIAction(handler: {_ in
-            let postVS = PostViewController()
-            self.navigationController?.pushViewController(postVS, animated: true)
-        }), for: .touchUpInside)
-        uiButton.setTitle("Go to post", for: .normal)
-        uiButton.setTitleColor(.white, for: .normal)
-        uiButton.translatesAutoresizingMaskIntoConstraints = false
-        uiButton.backgroundColor = .brown
-        
-        NSLayoutConstraint.activate([
-            uiButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            uiButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            uiButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            uiButton.heightAnchor.constraint(equalToConstant: 40)
-        ])
-        
-        
-        
-        
-        
+        view.backgroundColor = .lightGray
+        self.setupView().self
+    }
+    private func setupView() {
+        self.view.addSubview(self.verticalStackView)
+        self.verticalStackView.addArrangedSubview(postButtonOne)
+        self.verticalStackView.addArrangedSubview(postButtonTwo)
+        self.verticalStackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        self.verticalStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16).isActive = true
+        self.verticalStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16).isActive = true
+        self.verticalStackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        self.postButtonOne.leadingAnchor.constraint(equalTo: self.verticalStackView.leadingAnchor).isActive = true
+        self.postButtonOne.trailingAnchor.constraint(equalTo: self.verticalStackView.trailingAnchor).isActive = true
+        self.postButtonOne.topAnchor.constraint(equalTo: self.verticalStackView.topAnchor).isActive = true
+        self.postButtonTwo.leadingAnchor.constraint(equalTo: self.verticalStackView.leadingAnchor).isActive = true
+        self.postButtonTwo.trailingAnchor.constraint(equalTo: self.verticalStackView.trailingAnchor).isActive = true
+    }
+    
+    @objc func goToPost(sender:UIButton!)  {
+        let postViewController  = PostViewController()
+        self.navigationController?.pushViewController(postViewController, animated: true)
+        self.navigationItem.backButtonTitle = "Back"
     }
 }
--- Navigation/Navigation/ProfileViewController.swift
+++ Navigation/Navigation/ProfileViewController.swift
@@ -14,7 +14,18 @@ class ProfileViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
+    private lazy var newButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("New button", for: .normal)
+        button.backgroundColor = .systemPink
+        button.layer.cornerRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowOpacity = 0.7
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -22,22 +33,26 @@ class ProfileViewController: UIViewController {
         self.setupView()
         self.setupNavigationBar()
     }
-       
-    override func viewWillLayoutSubviews() {
-           super.viewWillLayoutSubviews()
-           self.profileHeaderView.frame = super.view.frame
-    }
-    
     private func setupNavigationBar() {
            self.navigationController?.navigationBar.prefersLargeTitles = false
            self.navigationItem.title = "Profile"
     }
     
     private func setupView() {
+        self.view.backgroundColor = .white
+        self.view.addSubview(self.profileHeaderView)
+        self.view.addSubview(self.newButton)
+        
         self.profileHeaderView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        self.profileHeaderView.topAnchor.constraint(equalTo:  self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        self.profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
         self.profileHeaderView.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor).isActive = true
         self.profileHeaderView.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        self.profileHeaderView.heightAnchor.constraint(equalTo: self.view.heightAnchor, constant: 220).isActive = true
         self.profileHeaderView.backgroundColor = .lightGray
+        
+        self.newButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        self.newButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.newButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.newButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
 }
